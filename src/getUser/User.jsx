0cc4e202ce4955fa61b42006c51153dacbdd867b
osmanforhad <osmanforhad93@gmail.com>
2025-03-29 17:37:04 +0600

    desgin user table with bootstrap and css

--- src/getUser/User.jsx
+++ src/getUser/User.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import './user.css';
 
 const User = () => {
   return (
-    <div>User Table</div>
+    <div className='userTable'>
+        <button type="button" class="btn btn-primary">Add User</button>
+        <table className='table table-bordered'>
+            <thead>
+                <tr>
+                    <th scope='col'>S.No</th>
+                    <th scope='col'>Name</th>
+                    <th scope='col'>Email</th>
+                    <th scope='col'>Addess</th>
+                    <th scope='col'>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>osman</td>
+                    <td>osman@gmail.com</td>
+                    <td>Feni</td>
+                    <td>Update || Delete</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
   )
 }
 
