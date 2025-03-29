b880f5704633cd3e9bb205cd2b3b9ac48fa936f5
osmanforhad <osmanforhad93@gmail.com>
2025-03-30 03:57:31 +0600

    writing script for sending user input from client side to server side

--- src/addUser/Adduser.jsx
+++ src/addUser/Adduser.jsx
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Adduser.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Adduser = () => {
-  
+    //define variable for user input
+    const users = {
+        name:"",
+        email:"",
+        address:"",
+    };
+    //set state for user input and navigation
+    const [user, setUser] = useState(users);
+    const navigate = useNavigate();
+    //function for handle user input
+    const inputHandler = (e) => {
+        const{name, value} = e.target;
+        console.log(name, value);
+        setUser({...user, [name]:value})
+    }
+    //function for send user input into db
+    const submitData = async(e) => {
+        try {
+            e.preventDefault();
+            await axios.post("http://localhost:8000/api/user", user)
+            .then((response) => {
+                console.log("User created Successfully");
+                navigate("/");
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
   return (
   <div className='addUser'>
     <Link to="/" type="button" className="btn btn-secondary">
         <i className="fa-solid fa-backward"></i> Back
         </Link>
     <h3>Add New User</h3>
-    <form className='addUserForm'>
+    <form className='addUserForm' onSubmit={submitData}>
         <div className='inputGroup'>
             <label htmlFor="name">Name:</label>
-            <input type="text" id='name' name='name' autoComplete='off' placeholder='Enter name here' />
+            <input type="text" id='name' name='name' onChange={inputHandler} autoComplete='off' placeholder='Enter name here' />
         </div>
         <div className='inputGroup'>
             <label htmlFor="email">Email:</label>
-            <input type="text" id='email' name='email' autoComplete='off' placeholder='Enter email here' />
+            <input type="text" id='email' name='email' onChange={inputHandler} autoComplete='off' placeholder='Enter email here' />
         </div>
         <div className='inputGroup'>
             <label htmlFor="address">Address:</label>
-            <input type="text" id='address' name='address' autoComplete='off' placeholder='Enter address here' />
+            <input type="text" id='address' name='address' onChange={inputHandler} autoComplete='off' placeholder='Enter address here' />
         </div>
         <div className='inputGroup'>
-        <button type="button" className="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary">Submit</button>
         </div>
     </form>
   </div>
